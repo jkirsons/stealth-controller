4adefe63c58e100a4cbea06f8599f624d23110e7
Jason <jkirsons@gmail.com>
2021-06-03 08:54:45 +0000

    ROS Updates

--- ros2/simplefoc_can_pubsub/simplefoc_can_pubsub/server_member_function.py
+++ ros2/simplefoc_can_pubsub/simplefoc_can_pubsub/server_member_function.py
@@ -1,19 +1,37 @@
 from simplefoc_interfaces.srv import GetValue
+from simplefoc_interfaces.srv import SetFloat
+from simplefoc_interfaces.srv import SetDouble
 
 import rclpy
 from rclpy.node import Node
+
+from .can_handler import CanHandler
+import can # https://python-can.readthedocs.io
 
 class MinimalService(Node):
 
   def __init__(self):
     super().__init__('minimal_service')
-    self.srv = self.create_service(GetValue, 'get_value', self.service_callback)
+    #self.srv = self.create_service(GetValue, 'get_value', self.service_callback)
+    self.srvFloat = self.create_service(SetFloat, 'set_float', self.service_callback_setFloat)
+    self.srvDouble = self.create_service(SetDouble, 'set_double', self.service_callback_setDouble)
 
-  def service_callback(self, request, response):
-    #response.sum = request.a + request.b
-    #self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
-    pass
+    self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=1000000)
+    self.handler = CanHandler(self.bus)
+    notifier = can.Notifier(self.bus, [ self.canReceived ])
 
+  def canReceived(self, msg):
+      self.handler.process(msg)
+
+  # https://docs.python.org/3/library/struct.html#format-characters
+  def service_callback_setFloat(self, request, response):
+    ba = bytearray(struct.pack("f", request.value)) 
+    self.handler.send_can_message(self.handler.data_type["float"], request.command, request.motor_id, request.bus_id, data)
+    return response
+
+  def service_callback_setDouble(self, request, response):
+    ba = bytearray(struct.pack("d", request.value)) 
+    self.handler.send_can_message(self.handler.data_type["double"], request.command, request.motor_id, request.bus_id, data)
     return response
 
 def main():
