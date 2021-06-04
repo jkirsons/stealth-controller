9c7816d1c02d3c0ceb6fd3cb4d43497a1766bcee
Jason <jkirsons@gmail.com>
2021-06-04 12:23:02 +0200

    Windows Env

---
+++ ros2/windows_env.bat
@@ -0,0 +1,6 @@
+cd c:\dev\dev_ws\
+call c:\dev\ros2-galactic\setup.bat
+call c:\dev\dev_ws\install\setup.bat
+set ROS_DOMAIN_ID=1
+set RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+set ROS_DISCOVERY_SERVER=192.168.1.26:11811
